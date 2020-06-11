9f0cb9b4d265661cc6af68d906ceedbd24579976
Dennis Wong <dennowong@gmail.com>
2020-06-10 17:33:04 -0700

    Update j1_calculator.js

--- js/j1_calculator/j1_calculator.js
+++ js/j1_calculator/j1_calculator.js
@@ -116,19 +116,19 @@ var j1Calculator = (function($) {
   }
 
   function parseTemplate(data) {
-    if (options.compiledTemplate) {
+    /*if (options.compiledTemplate) {
       var rendered = Handlebars.templates[options.handlebarsTemplateInternalName](data);
       $(options.mainContainer).html(rendered);
       addEvents();
     }
-    else {
+    else {*/
       $.get(options.templateLocation, function (template) {
         var compiled = Handlebars.compile(template);
         var rendered = compiled(data);
         $(options.mainContainer).html(rendered);
         addEvents();
       });
-    }
+    //}
   }
 
   function loadData() {
