f16d8690294f154f5b36ab26502e7cf7d631bcb4
Dennis Wong <dennowong@gmail.com>
2020-06-12 17:20:47 -0700

    Update program_calculator.js

--- js/program_calculator/program_calculator.js
+++ js/program_calculator/program_calculator.js
@@ -6,7 +6,7 @@ var programCalculator = (function($) {
     templateLocation: 'https://cdn.jsdelivr.net/gh/dennowong/globalinternships@latest/html/programCalculatorTemplate.html',
     handlebarsTemplateInternalName: 'programCalculatorTemplate.html',
     mainContainer: '#program-calculator',
-    csv_location: 'https://cdn.jsdelivr.net/gh/dennowong/globalinternships@latest/js/program_calculator/premium_prices.csv'
+    csv_location: 'https://uploads-ssl.webflow.com/5ec561ed25beadc20eb206da/5ee41baa7d4707a3b8a74795_premium_prices.csv'
   };
 
   function sortList(id) {
